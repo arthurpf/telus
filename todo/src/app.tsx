167a33eba8684992311218cf03eaa40ee01da744
Arthur Pavão <apavao@gazeus.com>
2024-06-04 19:48:09 -0300

    feat: setup initial MFE for todo app

---
+++ todo/src/app.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles/index.scss";
+
+const App = () => {
+  return <div>
+  teste
+ </div>
+};
+
+export default App;
+
---
+++ todo/webpack.config.js
@@ -0,0 +1,84 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = (_, argv) => ({
+  output: {
+    publicPath: "http://localhost:9001/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 9001,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          },
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          }
+        ]
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      }
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "todo",
+      filename: "todo-app.js",
+      remotes: {
+
+      },
+      exposes: {
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+          eager: true
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+          eager: true
+        },
+        "react-router-dom": {
+          singleton: true,
+          requiredVersion: deps["react-router-dom"],
+          eager: true
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+});
